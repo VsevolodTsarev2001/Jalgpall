13a0232ffc722bf2e1f15dd875436bbd917e57ff
opilane <opilane@HP-CZC2349HTY>
2024-09-25 11:18:58 +0300

    Etapp 2: mäng, pall ja staadion

---
+++ Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalgpall
+{
+    public class Game
+    {
+        //домашняя команда
+        public Team HomeTeam { get; }
+        //уездная команда
+        public Team AwayTeam { get; }
+        //стадион
+        public Stadium Stadium { get; }
+        //мяч
+        public Ball Ball { get; private set; }
+        //создание инры с домашней и уездной команды на стадионе
+        public Game(Team homeTeam, Team awayTeam, Stadium stadium)
+        {
+            HomeTeam = homeTeam;
+            homeTeam.Game = this;
+            AwayTeam = awayTeam;
+            awayTeam.Game = this;
+            Stadium = stadium;
+        }
+
+        public void Start()
+        {
+            Ball = new Ball(Stadium.Width / 2, Stadium.Height / 2, this);
+            HomeTeam.StartGame(Stadium.Width / 2, Stadium.Height);
+            AwayTeam.StartGame(Stadium.Width / 2, Stadium.Height);
+        }
+        //получаем позицую
+        private (double, double) GetPositionForAwayTeam(double x, double y)
+        {
+            return (Stadium.Width - x, Stadium.Height - y);
+        }
+        //получаем позицую в зависемости какая команда
+        public (double, double) GetPositionForTeam(Team team, double x, double y)
+        {
+            return team == HomeTeam ? (x, y) : GetPositionForAwayTeam(x, y);
+        }
+        //получаем позицую мяча для команд
+        public (double, double) GetBallPositionForTeam(Team team)
+        {
+            return GetPositionForTeam(team, Ball.X, Ball.Y);
+        }
+        //получаем скорости мяча для команд
+        public void SetBallSpeedForTeam(Team team, double vx, double vy)
+        {
+            if (team == HomeTeam)
+            {
+                //положительная
+                Ball.SetSpeed(vx, vy);
+            }
+            else
+            {
+                //отрицательная
+                Ball.SetSpeed(-vx, -vy);
+            }
+        }
+        //движение команд и мяча
+        public void Move()
+        {
+            HomeTeam.Move();
+            AwayTeam.Move();
+            Ball.Move();
+        }
+    }
+}
---
+++ Stadium.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalgpall
+{
+    public class Stadium
+    {
+        //размер стадиона
+        public Stadium(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; }
+
+        public int Height { get; }
+        //проверяем то что если находимся за пределами то false 
+        public bool IsIn(double x, double y)
+        {
+            return x >= 0 && x < Width && y >= 0 && y < Height;
+        }
+    }
+}
